63ee16b0053899937f08cc942f04b87e1736d976
feerlv <145036053+feerlv@users.noreply.github.com>
2023-10-17 08:46:56 -0600

    Create retoalimentos.py

---
+++ retoalimentos.py
@@ -0,0 +1,10 @@
+https://replit.com/join/tfwgllhlcg-maria-fernan900
+
+grasa = float(input("Dime el contendio de grasa del alimento: "))
+azucar = float(input("Dime el contenido de azucar del alimento: "))
+sodio = float(input("Dime el contenido de sodio del alimento: "))
+if grasa <= 5 and azucar <= 10 and sodio <= 100:
+  print("El alimento es bajo en sodio, azucar y grasas")
+else:
+  print("Se deben cosiderar advertencias nutricionales")
+  
